ee18a07f261efae9848888b4f9fc5a1c56db5b9b
Vinay Raikar <vinay.raikar@wipro.com>
2018-03-09 16:58:42 +0530

    Also get max runs from env.

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import time
 import os
 from colorprint import *
 
-MAX_RUNS = 100
+MAX_RUNS = int(os.environ.get('runs', '50'))
 message = os.environ.get('message', 'Hello')
 time_interval = 1
 count = 0
 
 while True:
-    if count > MAX_RUNS:
-        exit(1);
+    if count >= MAX_RUNS:
+        exit(1)
     print(message, color='blue')
     count += 1
     time.sleep(time_interval)
