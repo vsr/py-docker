fd07828dce97f0f55cae987229fa9ae3849574ff
Vinay Raikar <vinay.raikar@wipro.com>
2018-03-09 15:19:34 +0530

    Use environment variables for passing message.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from __future__ import print_function
 import time
+import os
 from colorprint import *
 
-time_interval = 3
+MAX_RUNS = 100
+message = os.environ.get('message', 'Hello')
+time_interval = 1
+count = 0
 
 while True:
-    print('Hello', color='blue')
+    if count > MAX_RUNS:
+        exit(1);
+    print(message, color='blue')
+    count += 1
     time.sleep(time_interval)
