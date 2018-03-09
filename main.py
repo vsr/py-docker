470272ef403bfb3c99ed80eabb53d7b8ade27b16
Vinay Raikar <vinay.raikar@wipro.com>
2018-03-09 13:04:04 +0530

    Use a library to print message.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from __future__ import print_function
 import time
+from colorprint import *
 
 time_interval = 3
 
 while True:
-    print('Hello')
+    print('Hello', color='blue')
     time.sleep(time_interval)
